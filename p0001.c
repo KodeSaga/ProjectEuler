1cda480938c86859e37f7aeef8d7ae8c0920570c
KodeSaga <kode.drama@gmail.com>
2019-03-26 18:34:32 +0530

    p0001 - Mathematical method added

--- p0001.c
+++ p0001.c
@@ -1,6 +1,16 @@
 //[Complete]Problem statement - Multiples of 3 and 5.
 #include<stdio.h>
 
+/* Method 1 - Mathematical using sum of 1 to n = n*(n+1)/2  */
+int main(){
+        int a = 1000,sum = 0;
+        int b = 999,count3 = b/3,count5 = b/5, count15 = b/15;
+        sum = (3*count3*(count3+1)/2) + (5*count5*(count5+1)/2) - (15*count15*(count15+1)/2)
+        printf("%d",sum);
+        return 0;
+}
+
+/* Method 2 - Simple Iterative approach
 int main(){
         int a = 1000,sum = 0;
         for (int i = 1; i < 1000; i++)
@@ -9,3 +19,4 @@ int main(){
         printf("%d",sum);
         return 0;
 }
+*/
