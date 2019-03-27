e721ae2009d1766db8c66014b53dadc3497c1245
KodeSaga <kode.drama@gmail.com>
2019-03-28 00:09:51 +0530

    p0005 started

---
+++ p0005.c
@@ -0,0 +1,15 @@
+//[Complete]Problem statement - Smallest multiple - smallest positive number that is evenly divisible
+// by all of the numbers from 1 to N
+
+// The problem can be termed as find the LCM of 1 to N
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+        int smallest_multiple;
+        
+
+
+        printf("%d",smallest_multiple);
+        return 0;
+}
